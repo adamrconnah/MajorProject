197af2478c869ababd377fbe18697d5ceada1115
unknown <adam.connah@yahoo.co.uk>
2013-02-05 21:07:15 +0000

    added test data to git

---
+++ testData/OntologyTerms.sql
@@ -0,0 +1,100 @@
+MP:0000001	 mammalian phenotype
+MP:0000002	 Morphology
+MP:0000003	 abnormal adipose tissue morphology
+MP:0000005	 increased brown adipose tissue amount
+MP:0000008	 increased white adipose tissue amount
+MP:0000010	 abnormal abdominal fat pad morphology
+MP:0000012	 loss of subcutaneous adipose tissue
+MP:0000013	 abnormal adipose tissue distribution
+MP:0000015	 abnormal ear pigmentation
+MP:0000017	 big ears
+MP:0000018	 small ears
+MP:0000019	 thick ears
+MP:0000020	 scaly ears
+MP:0000021	 prominent ears
+MP:0000022	 abnormal ear shape
+MP:0000023	 abnormal ear distance/ position
+MP:0000024	 lowered ear position
+MP:0000025	 otic hypertelorism
+MP:0000026	 abnormal inner ear morphology
+MP:0000027	 horizontal canal defects
+MP:0000028	 abnormal pars superior vestibularis morphology
+MP:0000029	 abnormal malleus morphology
+MP:0000030	 abnormal tympanic ring morphology
+MP:0000031	 abnormal cochlea morphology
+MP:0000032	 cochlear degeneration
+MP:0000033	 absent scala media
+MP:0000034	 abnormal vestibule morphology
+MP:0000035	 abnormal membranous labyrinth morphology
+MP:0000036	 absent semicircular canals
+MP:0000037	 abnormal lateral semicircular canal morphology
+MP:0000038	 thin semicircular canals
+MP:0000039	 abnormal otic capsule morphology
+MP:0000040	 absent middle ear ossicles
+MP:0000041	 absent endolymphatic duct
+MP:0000042	 abnormal organ of Corti morphology
+MP:0000043	 organ of Corti degeneration
+MP:0000044	 absent organ of Corti
+MP:0000045	 abnormal hair cell morphology
+MP:0000046	 abnormal sulcus ampullaris morphology
+MP:0000047	 abnormal interdental cells
+MP:0000048	 abnormal stria vascularis morphology
+MP:0000049	 abnormal middle ear morphology
+MP:0000051	 absent process brevus
+MP:0000052	 early ear unfolding
+MP:0000053	 excessive ear growth
+MP:0000054	 delayed ear emergence
+MP:0000060	 delayed bone ossification
+MP:0000061	 fragile skeleton
+MP:0000062	 increased bone mineral density
+MP:0000063	 decreased bone mineral density
+MP:0000064	 failure of secondary bone resorption
+MP:0000065	 abnormal bone marrow cavity morphology
+MP:0000066	 osteoporosis
+MP:0000067	 osteopetrosis
+MP:0000069	 kyphoscoliosis
+MP:0000071	 axial skeleton hypoplasia
+MP:0000073	 absent craniofacial bones
+MP:0000074	 abnormal neurocranium morphology
+MP:0000075	 absent neurocranium
+MP:0000077	 abnormal interparietal bone morphology
+MP:0000078	 abnormal supraoccipital bone morphology
+MP:0000079	 abnormal basioccipital bone morphology
+MP:0000080	 abnormal exoccipital bone morphology
+MP:0000081	 premature suture closure
+MP:0000082	 overlapping parietal bones
+MP:0000083	 ectopic cranial bone growth
+MP:0000084	 abnormal fontanelle morphology
+MP:0000085	 large anterior fontanelle
+MP:0000087	 absent mandible
+MP:0000088	 short mandible
+MP:0000090	 absent premaxilla
+MP:0000091	 short premaxilla
+MP:0000094	 absent alveolar process
+MP:0000097	 short maxilla
+MP:0000098	 abnormal vomer bone morphology
+MP:0000099	 absent vomer bone
+MP:0000100	 abnormal ethmoidal bone morphology
+MP:0000101	 absent ethmoidal bone
+MP:0000102	 abnormal nasal bone morphology
+MP:0000103	 nasal bone hypoplasia
+MP:0000104	 abnormal sphenoid bone morphology
+MP:0000105	 impaired ossification of basisphenoid bone
+MP:0000106	 abnormal basisphenoid bone morphology
+MP:0000107	 abnormal frontal bone morphology
+MP:0000108	 midline facial cleft
+MP:0000109	 abnormal parietal bone morphology
+MP:0000111	 cleft palate
+MP:0000114	 cleft chin
+MP:0000116	 abnormal tooth development
+MP:0000117	 absent tooth primordium
+MP:0000118	 arrest of tooth development
+MP:0000119	 abnormal tooth eruption
+MP:0000120	 malocclusion
+MP:0000121	 failure of tooth eruption
+MP:0000122	 accelerated tooth eruption
+MP:0000124	 absent teeth
+MP:0000125	 absent incisors
+MP:0000126	 brittle teeth
+MP:0000127	 degenerate molars
+MP:0000128	 growth retardation of molars
